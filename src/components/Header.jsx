119c9cd4a014b48ca66392e8a494a8c4cc3a5a5d
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-07 22:42:01 +0400

    ~

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ import Logo from "../assets/clip.svg?react";
 import MenuIcon from "../assets/menu.svg?react";
 
 const Header = ({ menuOn, setMenuOn }) => {
-  const handleMenuClick = () => setMenuOn(!menuOn);
+  const handleReload = () => window.location.reload();
+  const handleMenu = () => setMenuOn(!menuOn);
+
   return (
     <header>
-      <div className="flex">
+      <div className="flex pointer" onClick={handleReload}>
         <Logo className="logo" />
         <h1>Notes</h1>
       </div>
-      <MenuIcon className="icon" onClick={handleMenuClick} />
+      <MenuIcon className="icon pointer" onClick={handleMenu} />
     </header>
   );
 };
